fb5eea7aba6407e16ba3fe8904972d1a37162a02
Максим <brezgin1996@mail.ru>
2023-01-15 22:30:53 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,13 @@
 // функция расчёта
-function func() {
-    // переменная для результата
-  var result;
-  // получаем первое и второе число
-  var num1 = Number(document.getElementById("dm").value);
-  var num2 = Number(document.getElementById("Mydm").value);
-  // смотрим, что было в переменной с действием, и действуем исходя из этого
-  switch (op) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      result = num1 / num2;
-      break;
+button = document.querySelector('.summ')
+
+button.onclick = function() {
+    var result;
+    // получаем первое и второе число
+    var num1 = Number(document.getElementById("dm").value);
+    var num2 = Number(document.getElementById("Mydm").value);
+    // смотрим, что было в переменной с действием, и действуем исходя из этого
+    result = (num1*num1)/(num2*num2)
+  
+    document.getElementById("result").innerHTML = result;
   }
-
-  document.getElementById("result").innerHTML = result;
-
-}
