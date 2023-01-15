cb8c2d2aec416dcae8d0b9f993482aed4a02104d
Максим <brezgin1996@mail.ru>
2023-01-15 22:42:16 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ button = document.querySelector('.summ')
 
 button.onclick = function() {
     var result;
+    var promresult;
     // получаем первое и второе число
     var num1 = Number(document.getElementById("dm").value);
     var num2 = Number(document.getElementById("Mydm").value);
     // смотрим, что было в переменной с действием, и действуем исходя из этого
-    result = (num1*num1)/(num2*num2)
-  
+    promresult = Math.round((num2*num2)/(num1*num1)*100)/100
+    promresult = function(number){
+        return Math.round(parseFloat(number) * 100) / 100;
+    }
+    result == promresult
     document.getElementById("result").innerHTML = result;
   }
