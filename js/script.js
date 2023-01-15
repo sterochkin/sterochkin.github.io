af170016659dfed7b5b267bf81815dc69b274847
Максим <brezgin1996@mail.ru>
2023-01-15 23:03:18 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-// функция расчёта
+// Calc //
 button = document.querySelector('.summ')
-
 button.onclick = function() {
     var result;
-
-    // получаем первое и второе число
     var num1 = Number(document.getElementById("dm").value);
     var num2 = Number(document.getElementById("Mydm").value);
-    // смотрим, что было в переменной с действием, и действуем исходя из этого
     result = (num2*num2)/(num1*num1);
-
     document.getElementById("result").innerHTML = result;
-  }
+}
+// Clear //
+  clearButton = document.querySelector('.clearButton')
+  clearButton.onclick = function() {
+    document.getElementById("dm").value = "";
+    document.getElementById("Mydm").value = "";
+}
